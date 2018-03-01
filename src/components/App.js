0c62ea4ad690235354dca25482ea4cca3502aa98
Rafael Dias <rafael.dias@gmail.com>
2018-02-28 23:05:06 -0300

    fix imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from '../store';
 
-import TodoList from './components/TodoList';
+import TodoList from './TodoList';
 
 class App extends Component {
   
