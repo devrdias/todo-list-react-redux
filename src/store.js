852e27695de2271827c78312a7a970d162728e50
Rafael Dias <rafael.dias@gmail.com>
2018-03-05 23:16:43 -0300

    logger middleware

--- src/store.js
+++ src/store.js
@@ -18,7 +18,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
     composeEnhancers(
-        // applyMiddleware(logger)
+        applyMiddleware(logger)
     )
 );
 
